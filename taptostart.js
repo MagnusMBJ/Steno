5f1a541912637b4350af90c7f9dca0ad470e16d7
Mahdi <mahdi2010@live.dk>
2025-05-21 19:45:24 +0200

    Fix tap to start

--- taptostart.js
+++ taptostart.js
@@ -11,3 +11,16 @@ function hideStartScreen() {
 //touch og click to start med event listener der tjekker efter tryk/klik
 startSection.addEventListener('click', hideStartScreen);
 startSection.addEventListener('touchstart', hideStartScreen);
+
+// Loading skærm - viser at der sker noget i baggrunden
+function hideStartScreen() {
+    startSection.textContent = "Indlæser...";
+    startSection.style.fontSize = "3rem";
+    startSection.style.color = "#000000";
+    startSection.style.textAlign = "center";
+    startSection.style.fontFamily = "'Delicious Handrawn', cursive";
+    setTimeout(() => {
+        startSection.style.display = 'none';
+        vælgCirkelSection.style.display = 'flex';
+    }, 900); 
+}
